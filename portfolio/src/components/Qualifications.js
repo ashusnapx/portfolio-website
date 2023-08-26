22a3dc9bd5290415a54cd6db0d30a1041dc891c5
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-08-26 17:27:58 +0530

    Final Commit

--- portfolio/src/components/Qualifications.js
+++ portfolio/src/components/Qualifications.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const qualificationsData = [
     {
-        year: '2012 - 2017',
-        institution: 'Delhi Public School, Vindhyanagar',
-        description: 'Completed class 10th',
-        imageSrc: 'https://www.dpsjhakri.com/images/dps_logo1.png', // Replace with actual image URL
+        year: '2020 - 2024 (Expected)',
+        institution: 'Jabalpur Engineering College, Jabalpur',
+        description: 'Bachelor of Technology in Information Technology',
+        imageSrc: 'https://upload.wikimedia.org/wikipedia/en/f/f2/Jabalpur_Engineering_College_%28JEC%29%27s_logo.png', // Replace with actual image URL
     },
     {
         year: '2017 - 2019',
@@ -14,10 +14,10 @@ const qualificationsData = [
         imageSrc: 'https://pbs.twimg.com/profile_images/1295831328/Logo_LBS_Group_copy_400x400.jpg', // Replace with actual image URL
     },
     {
-        year: '2020 - 2024 (Expected)',
-        institution: 'Jabalpur Engineering College, Jabalpur',
-        description: 'Bachelor of Technology in Information Technology',
-        imageSrc: 'https://upload.wikimedia.org/wikipedia/en/f/f2/Jabalpur_Engineering_College_%28JEC%29%27s_logo.png', // Replace with actual image URL
+        year: '2012 - 2017',
+        institution: 'Delhi Public School, Vindhyanagar',
+        description: 'Completed class 10th',
+        imageSrc: 'https://www.dpsjhakri.com/images/dps_logo1.png', // Replace with actual image URL
     },
     // Add more qualifications...
 ];
