4f8b3c8d46dfc8cbf36f86136f55025f15d66476
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-12-08 13:22:46 +0530

    skills updated!

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -6,12 +6,16 @@ import {
   FaReact,
   FaPython,
   FaNodeJs,
-  FaServer,
-  FaDatabase
 } from 'react-icons/fa';
 import { DiJavascript1 } from 'react-icons/di';
 import { AiFillDatabase } from 'react-icons/ai';
+import { GiTeamIdea } from "react-icons/gi";
+import { RiEnglishInput } from "react-icons/ri";
 import { LucideHammer } from 'lucide-react';
+import { SiGooglecloud, SiFuturelearn, SiVisualstudiocode, SiPandas, SiPlotly, SiNumpy, SiMongodb, SiExpress, SiTailwindcss } from "react-icons/si";
+import { BsFillPatchQuestionFill } from "react-icons/bs";
+import { FaGitlab } from "react-icons/fa";
+import { TbBrandTypescript } from "react-icons/tb";
 
 const Skills = () => {
   useEffect( () => {
@@ -28,6 +32,9 @@ const Skills = () => {
           <LucideHammer className="text-4xl font-bold mb-8 text-black/50 text-center w-10 h-8" />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+
+
           <div className="bg-white/40 p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-semibold mb-4 text-indigo-600">
               Programming Languages 
@@ -42,35 +49,94 @@ const Skills = () => {
               <li className="mb-2 flex items-center">
                 <FaPython className="mr-2" /> Python
               </li>
-              <li className="flex items-center">
+              <li className="mb-2 flex items-center">
                 <DiJavascript1 className="mr-2" /> JavaScript
+              </li>
+              <li className="flex items-center">
+                <TbBrandTypescript className="mr-2" /> Typescript
               </li>
             </ul>
           </div>
+
+
+
+
           <div className="bg-white/40  p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-semibold mb-4 text-green-600">Frontend Development</h2>
             <ul className="list-disc pl-6 text-gray-800">
               <li className="mb-2"><FaHtml5 className="inline mr-2" /> HTML/HTML5</li>
               <li className="mb-2"><FaCss3Alt className="inline mr-2" /> CSS/CSS3</li>
               <li className="mb-2"><FaReact className="inline mr-2" /> ReactJS</li>
-              <li><FaCss3Alt className="inline mr-2" /> Tailwind CSS</li>
+              <li><SiTailwindcss className="inline mr-2" /> Tailwind CSS</li>
             </ul>
           </div>
+
+
+
+
+
           <div className="bg-white/40 p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-semibold mb-4 text-blue-600">Backend Development</h2>
             <ul className="list-disc pl-6 text-gray-800">
               <li className="mb-2"><FaNodeJs className="inline mr-2" /> Node.js</li>
-              <li className="mb-2"><FaServer className="inline mr-2" /> Express.js</li>
-              <li className="mb-2"><FaDatabase className="inline mr-2" /> MongoDB</li>
+              <li className="mb-2"><SiExpress className="inline mr-2" /> Express.js</li>
+              <li className="mb-2"><SiMongodb className="inline mr-2" /> MongoDB</li>
             </ul>
           </div>
+
+
+
+
           <div className="bg-white/40 p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-semibold mb-4 text-green-600">Data Analysis</h2>
             <ul className="list-disc pl-6 text-gray-800">
-              <li className="mb-2"><FaPython className="inline mr-2" /> Numpy</li>
-              <li className="mb-2"><FaPython className="inline mr-2" /> Pandas</li>
+              <li className="mb-2"><SiNumpy className="inline mr-2" /> Numpy</li>
+              <li className="mb-2"><SiPandas className="inline mr-2" /> Pandas</li>
               <li className="mb-2"><AiFillDatabase className="inline mr-2" /> Bokeh</li>
-              <li><AiFillDatabase className="inline mr-2" /> Plotly</li>
+              <li><SiPlotly className="inline mr-2" /> Plotly</li>
+            </ul>
+          </div>
+
+
+          <div className="bg-white/40 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold mb-4 text-indigo-600">
+              Cloud Services
+            </h2>
+            <ul className="list-disc pl-6 text-gray-800">
+              <li className="list-disc mb-2 flex items-center">
+                <SiGooglecloud className="mr-2" /> Google Cloud Service
+              </li>
+            </ul>
+          </div>
+
+
+          <div className="bg-white/40  p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold mb-4 text-green-600">Development Tools</h2>
+            <ul className="list-disc pl-6 text-gray-800">
+              <li className="mb-2"><FaGitlab className="inline mr-2" /> GitLab</li>
+            </ul>
+          </div>
+
+          <div className="bg-white/40 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold mb-4 text-indigo-600">
+              Communication & Collaboration
+            </h2>
+            <ul className="list-disc pl-6 text-gray-800">
+              <li className="mb-2 flex items-center">
+                <RiEnglishInput className="mr-2" /> Fluent in english
+              </li>
+              <li className="mb-2 flex items-center">
+                <BsFillPatchQuestionFill className="mr-2" /> Proactive and asking questions
+              </li>
+              <li className="mb-2 flex items-center">
+                <GiTeamIdea className="mr-2" /> Team player
+              </li>
+              <li className="mb-2 flex items-center">
+                <SiVisualstudiocode className="mr-2" /> Documenting the codebase
+              </li>
+              <li className="flex items-center">
+                <SiFuturelearn className="mr-2" /> Daily learner
+              </li>
             </ul>
           </div>
         </div>
