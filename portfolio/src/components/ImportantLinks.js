ede9ceacbc4327200f90a64236d1df35da630b07
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2024-02-25 14:45:14 +0530

    some optimization

--- portfolio/src/components/ImportantLinks.js
+++ portfolio/src/components/ImportantLinks.js
@@ -3,38 +3,52 @@ import React, { useEffect } from 'react';
 import { linksData } from '../constants/constants';
 
 const ImportantLinks = () => {
-    useEffect( () => {
-        window.scrollTo( 0, 0 );
-    }, [] );
-    return (
-        <div className="bg-gray-100 min-h-screen py-16 flex flex-col">
-            <div className="container mx-auto text-center">
-                <div className='flex items-center justify-center space-x-2'>
-                    <h1 className="text-4xl font-bold text-black/50 mb-10">Important Links</h1>
-                    <LucideMousePointerClick className='w-10 h-9 text-black/50 mb-10'/>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {linksData.map( ( link ) => (
-                        <div key={link.name} className="bg-white rounded-md shadow-md p-6 flex flex-col">
-                            <div className="h-20 mb-4 flex items-center justify-center">
-                                <img src={link.logo} alt={link.name} className="h-full  p-1 rounded-md" />
-                            </div>
-                            <h2 className="text-xl font-semibold mb-2 text-black">{link.name}</h2>
-                            <p className="text-gray-500 mb-4 line-clamp-3">{link.description}</p>
-                            <a
-                                href={link.link}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="flex items-center bg-blue-500 text-white py-2 px-4 rounded-md justify-center hover:bg-blue-600 transition duration-300"
-                            >
-                                <LucideLink2 className='mr-2'/> Link to {link.name}'s profile
-                            </a>
-                        </div>
-                    ) )}
-                </div>
-            </div>
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div className='bg-gray-100 min-h-screen py-16 flex flex-col'>
+      <div className='container mx-auto text-center'>
+        <div className='flex items-center justify-center space-x-2'>
+          <h1 className='text-4xl font-bold text-black/50 mb-10'>
+            Important Links
+          </h1>
+          <LucideMousePointerClick className='w-10 h-9 text-black/50 mb-10' />
         </div>
-    );
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          {linksData.map((link) => (
+            <div
+              key={link.name}
+              className='bg-white rounded-md shadow-md p-6 flex flex-col'
+            >
+              <div className='h-20 mb-4 flex items-center justify-center'>
+                <img
+                  src={link.logo}
+                  alt={link.name}
+                  className='h-full  p-1 rounded-md'
+                  decoding='async'
+                />
+              </div>
+              <h2 className='text-xl font-semibold mb-2 text-black'>
+                {link.name}
+              </h2>
+              <p className='text-gray-500 mb-4 line-clamp-3'>
+                {link.description}
+              </p>
+              <a
+                href={link.link}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='flex items-center bg-blue-500 text-white py-2 px-4 rounded-md justify-center hover:bg-blue-600 transition duration-300'
+              >
+                <LucideLink2 className='mr-2' /> Link to {link.name}'s profile
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImportantLinks;
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -5,59 +5,56 @@ import ProgressiveImage from 'react-progressive-graceful-image';
 import { projectsData } from '../constants/constants';
 
 const Projects = () => {
-  useEffect( () => {
-    window.scrollTo( 0, 0 );
-  }, [] );
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
-    <div className="relative min-h-screen bg-gray-100">
-      <div className="container mx-auto py-12 px-4">
+    <div className='relative min-h-screen bg-gray-100'>
+      <div className='container mx-auto py-12 px-4'>
         <div className='flex space-x-2 items-center justify-center'>
-          <h1 className="text-4xl text-center font-bold text-black/50 mb-8">Projects</h1>
-          <LucideProjector className=" text-black/50 mb-8 w-10 h-9 " />
+          <h1 className='text-4xl text-center font-bold text-black/50 mb-8'>
+            Projects
+          </h1>
+          <LucideProjector className=' text-black/50 mb-8 w-10 h-9 ' />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {projectsData.map( ( project ) => (
-            <div key={project.id} className="bg-white rounded-md shadow-md p-4 ">
-              <ProgressiveImage
-                delay={300}
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          {projectsData.map((project) => (
+            <div
+              key={project.id}
+              className='bg-white rounded-md shadow-md p-4 '
+            >
+              <img
                 src={project.imageSrc}
-                placeholder="tiny-image.jpg"
-              >
-                {( src, loading ) => (
-                  <img
-                    src={src}
-                    alt={project.name}
-                    loading={loading ? 'lazy' : 'eager'}
-                    className="w-full h-48 object-cover rounded-md mb-4 shadow-xl"
-                  />
-                )}
-              </ProgressiveImage>
-              <h2 className="text-xl font-semibold mb-2 text-black">
+                alt={project.name}
+                className='w-full h-48 object-cover rounded-md mb-4 shadow-xl'
+                decoding='async'
+              />
+              <h2 className='text-xl font-semibold mb-2 text-black'>
                 {project.name}
               </h2>
-              <p className="text-gray-500 mb-2">{project.techStacks}</p>
-              <p className="text-gray-800">{project.description}</p>
-              <div className="mt-4 flex space-x-4">
+              <p className='text-gray-500 mb-2'>{project.techStacks}</p>
+              <p className='text-gray-800'>{project.description}</p>
+              <div className='mt-4 flex space-x-4'>
                 <a
                   href={project.githubLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='flex items-center bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300'
                 >
-                  <FaGithub className="mr-2" /> GitHub
+                  <FaGithub className='mr-2' /> GitHub
                 </a>
                 <a
                   href={project.liveLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='flex items-center bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300'
                 >
-                  <FaExternalLinkAlt className="mr-2" /> Live
+                  <FaExternalLinkAlt className='mr-2' /> Live
                 </a>
               </div>
             </div>
-          ) )}
+          ))}
         </div>
       </div>
     </div>
